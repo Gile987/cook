ccaf8a48b00896030d2c130ef836b7123938b0ee
gile987 <mladen.reljic87@gmail.com>
2023-08-19 17:44:45 +0200

    Add an ingredients array to the ingredients model, utilize it to add ingredients to recipes and display them

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,19 +1,22 @@
 import { EventEmitter } from '@angular/core';
 import { Recipe } from './recipe.model';
+import { Ingredient } from '../shared/models/ingredient.model';
 
 export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
 
   private recipes: Recipe[] = [
     new Recipe(
-      'Test Recipe',
-      'Test Test Test',
-      'https://get.pxhere.com/photo/restaurant-dish-meal-food-produce-macro-plate-meat-lunch-cuisine-steak-asian-food-food-photography-hors-d-oeuvre-food-plating-946936.jpg'
+      'Meat Cheese',
+      'A tasty combination of meat and cheese',
+      'https://get.pxhere.com/photo/restaurant-dish-meal-food-produce-macro-plate-meat-lunch-cuisine-steak-asian-food-food-photography-hors-d-oeuvre-food-plating-946936.jpg',
+      [new Ingredient('Meat', 1), new Ingredient('Cheese', 20)]
     ),
     new Recipe(
-      'Test Recipe 2',
-      'Test Test Test 2',
-      'https://get.pxhere.com/photo/restaurant-dish-meal-food-produce-macro-plate-meat-lunch-cuisine-steak-asian-food-food-photography-hors-d-oeuvre-food-plating-946936.jpg'
+      'Burek',
+      'Zna se sta je burek',
+      'https://get.pxhere.com/photo/restaurant-dish-meal-food-produce-macro-plate-meat-lunch-cuisine-steak-asian-food-food-photography-hors-d-oeuvre-food-plating-946936.jpg',
+      [new Ingredient('Meat', 3), new Ingredient('Testo', 5)]
     ),
   ];
 
